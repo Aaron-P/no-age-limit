c42f87e93edcb8bd2ebb42e9948c31b17ce2cb8b
sabir <manandharsabir@hotmail.com>
2017-11-26 15:53:18 +0545

    few sec timeout to decide the restriction

--- modifier.js
+++ modifier.js
@@ -1,11 +1,10 @@
 var player = document.getElementById( "player-unavailable" );
-var sign_in_request = document.querySelector( "ytd-button-renderer.ytd-player-error-message-renderer" );
 
 if ( player && player.offsetParent !== null ) {
 	var page_class = document.getElementById( "page" ).className,
 		regexp = /video-([\w-_]*)/,
 		iframe = document.createElement( "iframe" );
-		
+
 	iframe.setAttribute( "id", "no-age-limit-iframe" );
 	iframe.setAttribute( "width", player.clientWidth );
 	iframe.setAttribute( "height", player.clientHeight );
@@ -23,43 +22,49 @@ if ( player && player.offsetParent !== null ) {
 		iframe.setAttribute( "width", player.clientWidth );
 		iframe.setAttribute( "height", player.clientHeight );
 	} );
-} else if ( sign_in_request ) {
-	var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver,
-		player = document.getElementById( "player-container" ),
-		video_id = document.querySelector( "ytd-watch" ).getAttribute('video-id'),
-		iframe = document.createElement( "iframe" );
+} else {
+	setTimeout( function () {
+		var sign_in_request = document.querySelector( "ytd-button-renderer.ytd-player-error-message-renderer" );
 
-	var observer = new MutationObserver(function(mutations) {
-		mutations.forEach(function(mutation) {
-			if (mutation.type == "attributes") {
+		if ( sign_in_request ) {
+			var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver,
+				player = document.getElementById( "player-container" ),
+				video_id = document.querySelector( "ytd-watch" ).getAttribute('video-id'),
+				iframe = document.createElement( "iframe" );
+
+			var observer = new MutationObserver(function(mutations) {
+				mutations.forEach(function(mutation) {
+					if (mutation.type == "attributes") {
+						iframe = document.getElementById( "no-age-limit-iframe" );
+
+						iframe.remove();
+					}
+				});
+			});
+
+			observer.observe(document.querySelector( "ytd-watch" ), {
+				attributes: true
+			});
+
+			iframe.setAttribute( "id", "no-age-limit-iframe" );
+			iframe.setAttribute( "width", player.clientWidth );
+			iframe.setAttribute( "height", player.clientHeight );
+			iframe.setAttribute( "src", "https://www.youtube.com/embed/" + video_id + "?autoplay=1" );
+			iframe.setAttribute( "frameborder" , 0);
+			iframe.setAttribute( "allowFullScreen", "" );
+			iframe.style.position = 'relative';
+			iframe.style.bottom = player.clientHeight + 'px';
+
+			player.parentElement.appendChild( iframe );
+
+			window.addEventListener( "resize", function() {
+				var player = document.getElementById( "player-container" )
 				iframe = document.getElementById( "no-age-limit-iframe" );
 
-				iframe.remove();
-			}
-		});
-	});
-
-	observer.observe(document.querySelector( "ytd-watch" ), {
-		attributes: true
-	});
-
-	iframe.setAttribute( "id", "no-age-limit-iframe" );
-	iframe.setAttribute( "width", player.clientWidth );
-	iframe.setAttribute( "height", player.clientHeight );
-	iframe.setAttribute( "src", "https://www.youtube.com/embed/" + video_id + "?autoplay=1" );
-	iframe.setAttribute( "frameborder" , 0);
-	iframe.setAttribute( "allowFullScreen", "" );
-	iframe.style.position = 'relative';
-	iframe.style.bottom = player.clientHeight + 'px';
-
-	player.parentElement.appendChild( iframe );
-
-	window.addEventListener( "resize", function() {
-		var player = document.getElementById( "player-container" )
-		iframe = document.getElementById( "no-age-limit-iframe" );
-
-		iframe.setAttribute( "width", player.clientWidth );
-		iframe.setAttribute( "height", player.clientHeight );
-		iframe.style.bottom = player.clientHeight + 'px';
-	} );
+				iframe.setAttribute( "width", player.clientWidth );
+				iframe.setAttribute( "height", player.clientHeight );
+				iframe.style.bottom = player.clientHeight + 'px';
+			} );
+		}
+	}, 1000 );
 }
