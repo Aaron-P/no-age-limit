de5f8b7bfb36aa48053ed4bc5e6d403baa2b6a1b
sabir <manandharsabir@hotmail.com>
2017-11-26 15:35:47 +0545

    patch for youtube desktop app

--- modifier.js
+++ modifier.js
@@ -1,4 +1,5 @@
 var player = document.getElementById( "player-unavailable" );
+var sign_in_request = document.querySelector( "ytd-button-renderer.ytd-player-error-message-renderer" );
 
 if ( player.offsetParent !== null ) {
 	var page_class = document.getElementById( "page" ).className,
@@ -18,8 +19,47 @@ if ( player.offsetParent !== null ) {
 
 	window.addEventListener( "resize", function() {
 		var player = document.getElementById( "player-unavailable" )
-		    iframe = document.getElementById( "no-age-limit-iframe" );
+			iframe = document.getElementById( "no-age-limit-iframe" );
 		iframe.setAttribute( "width", player.clientWidth );
 		iframe.setAttribute( "height", player.clientHeight );
+	} );
+} else if ( sign_in_request ) {
+	var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver,
+		player = document.getElementById( "player-container" ),
+		video_id = document.querySelector( "ytd-watch" ).getAttribute('video-id'),
+		iframe = document.createElement( "iframe" );
+
+	var observer = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+			if (mutation.type == "attributes") {
+				iframe = document.getElementById( "no-age-limit-iframe" );
+
+				iframe.remove();
+			}
+		});
+	});
+
+	observer.observe(document.querySelector( "ytd-watch" ), {
+		attributes: true
+	});
+
+	iframe.setAttribute( "id", "no-age-limit-iframe" );
+	iframe.setAttribute( "width", player.clientWidth );
+	iframe.setAttribute( "height", player.clientHeight );
+	iframe.setAttribute( "src", "https://www.youtube.com/embed/" + video_id + "?autoplay=1" );
+	iframe.setAttribute( "frameborder" , 0);
+	iframe.setAttribute( "allowFullScreen", "" );
+	iframe.style.position = 'relative';
+	iframe.style.bottom = player.clientHeight + 'px';
+
+	player.parentElement.appendChild( iframe );
+
+	window.addEventListener( "resize", function() {
+		var player = document.getElementById( "player-container" )
+		iframe = document.getElementById( "no-age-limit-iframe" );
+
+		iframe.setAttribute( "width", player.clientWidth );
+		iframe.setAttribute( "height", player.clientHeight );
+		iframe.style.bottom = player.clientHeight + 'px';
 	} );
 }
